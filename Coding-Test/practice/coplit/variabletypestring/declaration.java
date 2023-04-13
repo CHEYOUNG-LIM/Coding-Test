4ff87032ee185b2bb998a432a5bb1e1ac288ca13
CHEYOUNG-LIM <66333269+CHEYOUNG-LIM@users.noreply.github.com>
2023-04-13 23:34:38 +0900

    Delete Coding-Test directory

--- Coding-Test/practice/coplit/variabletypestring/declaration.java
+++
@@ -1,9 +0,0 @@
-//변수 선언하기(declaration)
-//키워드 char을 사용하여 변수 course를 선언하세요.
-
-package coplit.variabletypestring;
-public class declaration {
-    public static void main(String[] args) {
-        char course;
-    }
-}
