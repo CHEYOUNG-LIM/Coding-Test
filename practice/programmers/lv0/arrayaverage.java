2c843462ab057f9dee55cbbbf4b13073e46d1377
CHEYOUNG-LIM <66333269+CHEYOUNG-LIM@users.noreply.github.com>
2023-04-13 23:34:14 +0900

    Add files via upload

---
+++ practice/programmers/lv0/arrayaverage.java
@@ -0,0 +1,20 @@
+//배열의 평균값
+//정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+
+package programmers.lv0;
+
+class arrayaverage {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        int sum = 0;
+
+        for (int i = 0 ; i < numbers.length ; i++) {
+            sum = sum + numbers[i];
+            answer = (double)sum / numbers.length;
+        }
+
+        return answer;
+    }
+}
+
+// 쉬움
