94bd2b184c258ee267875188f6c4408ab6836fb4
CHEYOUNG-LIM <66333269+CHEYOUNG-LIM@users.noreply.github.com>
2023-06-15 22:41:08 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        double A, B;
+        double C;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = A / B;
+        System.out.println(C);
+    }
+}
