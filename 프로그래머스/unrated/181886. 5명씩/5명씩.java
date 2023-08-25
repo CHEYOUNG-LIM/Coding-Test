11b43b64a3cdef6d67b353ac8fd5b6e1212a3ff1
CHEYOUNG-LIM <66333269+CHEYOUNG-LIM@users.noreply.github.com>
2023-08-26 00:09:28 +0900

    [unrated] Title: 5명씩, Time: 0.01 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -1,12 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public String[] solution(String[] names) {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < names.length; i+=5) {
-          list.add(names[i]);
+        String[] answer = {};
+        if(names.length % 5 == 0) {
+            answer = new String[(names.length / 5)];
+        } else {
+            answer = new String[(names.length / 5) + 1];
         }
-        return list.toArray(new String[0]);
+        for(int i = 0; i < names.length; i=i+5) {
+            answer[i/5] = names[i];
+        }
+        return answer;
     }
 }
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// class Solution {
+//     public String[] solution(String[] names) {
+//         List<String> list = new ArrayList<>();
+//         for (int i = 0; i < names.length; i+=5) {
+//           list.add(names[i]);
+//         }
+//         return list.toArray(new String[0]);
+//     }
+// }
