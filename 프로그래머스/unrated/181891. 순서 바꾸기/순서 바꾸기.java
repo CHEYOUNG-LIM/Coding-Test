2bd36c59d8d59df4d5f2df3f699681d3ce7c20dd
CHEYOUNG-LIM <66333269+CHEYOUNG-LIM@users.noreply.github.com>
2023-08-26 03:29:25 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,26 @@
+// class Solution {
+//     public int[] solution(int[] num_list, int n) {
+//         int[] answer = new int[num_list.length];
+//         int index = 0;
+
+//         for (int i = n; i < num_list.length; i++) {
+//             answer[index++] = num_list[i];
+//         }
+//         for (int i = 0; i < n; i++) {
+//             answer[index++] = num_list[i];
+//         }
+
+//         return answer;
+//     }
+// }
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        
+        System.arraycopy(num_list, n, answer, 0, num_list.length -n);
+        System.arraycopy(num_list, 0, answer, num_list.length -n, n);
+        
+        return answer;
+    }
+}
