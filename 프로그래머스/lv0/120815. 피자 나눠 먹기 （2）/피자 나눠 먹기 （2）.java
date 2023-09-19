89238786cf74910178a633056387da0aee4e73e5
CHEYOUNG-LIM <66333269+CHEYOUNG-LIM@users.noreply.github.com>
2023-09-19 23:43:33 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-        for(int i = 1; i <= n; i++) {
-            double a = (double)(6 * i) / n;
-            if(a % 1 == 0) {
-                answer = i;
-                break;
-            }
+        int answer = 1;
+
+        while(true){
+            if(6*answer%n==0) break;
+            answer++;
         }
+
         return answer;
     }
 }
