43cb3f9ed5f1482d967edd7b71bbe6eee7901fbb
CHEYOUNG-LIM <66333269+CHEYOUNG-LIM@users.noreply.github.com>
2023-09-21 23:34:26 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String a = Integer.toString(order);
+        
+        for(int i = 0; i < a.length() ; i++) {
+            if(a.charAt(i) == '3' || a.charAt(i) == '6' || a.charAt(i) == '9') {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
