860b089f44b9d784f6bfc8ed9b0e10aae0c801d5
CHEYOUNG-LIM <66333269+CHEYOUNG-LIM@users.noreply.github.com>
2023-11-03 22:32:12 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        for(int i = 0; i < numbers.length; i++) {
+            answer = answer - numbers[i];
+        }
+
+        return answer;
+    }
+}
+
+// class Solution {
+//     public int solution(int[] numbers) {
+//         int answer = 0;
+//         int[] answ = new int[10];
+        
+//         for(int i=0; i<numbers.length; i++) {
+//             answ[numbers[i]] = 1;
+//         }
+
+//         for(int i=0; i<answ.length; i++) {
+//             if(answ[i] == 0) answer += i;
+//         }
+        
+        
+        
+//         return answer;
+//     }
+// }
